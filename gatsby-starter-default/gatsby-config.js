fc8bf7d6fe53adf25c12d175c30fc3db68f051d5
Alicia Vissuet <alicia@rideos.ai>
2020-07-20 12:22:26 -0700

    Add ability to make changes in cms

--- gatsby-starter-default/gatsby-config.js
+++ gatsby-starter-default/gatsby-config.js
@@ -27,6 +27,21 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/static/assets`,
+        name: 'images',
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/content`,
+      },
+    },
+    'gatsby-transformer-remark',
     `gatsby-plugin-netlify-cms`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
