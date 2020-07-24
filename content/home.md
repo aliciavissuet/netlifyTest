0b2c54e6eef19fc9b86cbfa7e73816aa3d4efef8
Alicia Vissuet <alicia@rideos.ai>
2020-07-24 14:40:31 -0700

    POC editing

--- content/home.md
+++ content/home.md
@@ -2,4 +2,8 @@
 title: Changing the title!
 intro: Let's change the title
 image: assets/cute-dog.jpg
+solidButtonLink: www.google.com
+solidButtonText: API Documentation
+outlinedButtonLink: www.google.com
+outlinedButtonText: API Documentation
 ---
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { graphql } from 'gatsby';
-
+import './styles.css'
 export const query = graphql`
   query {
     allFile (filter: {sourceInstanceName: {eq: "content"} name: {eq: "home"}}) {
@@ -15,6 +15,10 @@ export const query = graphql`
               title
               intro
               image
+              outlinedButtonText
+              outlinedButtonLink
+              solidButtonText
+              solidButtonLink
           }
         }
       }
@@ -24,19 +28,23 @@ export const query = graphql`
 
 const IndexPage = (props) => {
   const data = props.data.allFile.edges[0].node.childMarkdownRemark.frontmatter
-  console.log(data)
   return (
-    <Layout>
+    <div className={'pocContainer'}>
       <SEO title="Home" />
-      <h1>{data.title} is the title</h1>
-      <p>{data.intro}</p>
-      <p>Now go build something great.</p>
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-        <img src={data.image} alt={data.imageAltTag} />
+      <img src={data.image} alt={data.imageAltTag} className="image" />
+      <div className="contentDiv d">
+        <h1 className="title">{data.title}</h1>
+        <p className="bodyParagraph">{data.intro}</p>
+        <div className="buttonsDiv">
+          <a className="buttonOutline" to={data.outlinedButtonLink}>
+            {data.outlinedButtonText}
+          </a>
+          <a className="buttonSolid" to={data.outlinedButtonLink}>
+            {data.outlinedButtonText}
+          </a>
+        </div>
       </div>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </Layout>
+    </div>
   )
 }
 
